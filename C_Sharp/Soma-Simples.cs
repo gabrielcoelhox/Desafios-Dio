d20dc20047b09fb125ac80bae4262e4a4028c308
unknown <gabriel.coelhox@gmail.com>
2021-06-02 20:18:58 -0300

    Desafios csharp

---
+++ C_Sharp/Soma-Simples.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Declare Variables
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        A = 30
+        B = 10
+
+        //Insira as variaveis de acordo com o solicitado
+        int SOMA = (A + );
+
+        //Print
+        Console.WriteLine("SOMA = ", SOMA);
+
+    }
+}

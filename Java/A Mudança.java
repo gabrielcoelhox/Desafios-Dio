eaf71a9e3f3a6cf2d720ce1685b107dae4b2783d
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-23 17:32:57 -0300

    A Mudança

---
+++ Java/A Mudança.java
@@ -0,0 +1,44 @@
+/* DESAFIO:
+Hermione está criando um novo Vira Tempo especialmente para programadores. É
+impressionante as vantagens que ele oferece e o conforto pra codar que ele tem.
+O artefato ainda está em desenvolvimento e ele prometeu consertar os bugs e
+colocar uns apetrechos melhores e, em troca, pediu um sistema simples para o
+modo Standy Bay. O problema é que o artefato por si só sempre tem o ângulo de
+inclinação do Sol/Lua (de 0 a 360). Valendo um Vira Tempo, caso deseja aceitar:
+dada em grau da inclinação do Sol/Lua, informe em qual período do dia ele se
+encontra.
+
+ENTRADA:
+A entrada contém um número inteiro M (0 ≤ M ≤ 360) representando o grau do
+Sol/Lua. Como a posição muda constantemente seu programa receberá diversos
+casos a cada segundo (EOF).
+
+SAÍDA:
+Imprima uma saudação referente ao período do dia que ele se encontra: "Boa
+Tarde!!", "Boa Noite!!", "Bom Dia!!" e "De Madrugada!!".
+*/
+
+package application;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	while (sc.hasNext()) {
+    		int graus = sc.nextInt();
+    		if (graus == 360 || graus >= 0 && graus < 90) { 
+    			System.out.println("Bom Dia!!");
+    		} else if (graus >= 90 && graus < 180) {
+    			System.out.println("Boa Tarde!!");
+    		} else if (graus >= 180 && graus < 270) {
+    			System.out.println("Boa Noite!!");
+    		} else {
+    			System.out.println("De Madrugada!!");
+    		}
+    	}
+   }
+}   
+
